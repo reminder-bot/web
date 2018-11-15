53675ce7b300b41719f27efb61a81ecb82f09971
jude-LafiteIII <judewrs@gmail.com>
2018-11-15 17:16:48 +0000

    removed the server side sessions

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,12 @@ from flask import Flask
 from config import Config
 from flask_dance.contrib.discord import make_discord_blueprint, discord
 from flask_sqlalchemy import SQLAlchemy
-from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
 discord_blueprint = make_discord_blueprint(scope=['identify', 'guilds'], redirect_url='dashboard')
 app.register_blueprint(discord_blueprint, url_prefix='/login')
 db = SQLAlchemy(app)
-Session(app)
 
 from app import routes, models
 
--- app/models.py
+++ app/models.py
@@ -32,17 +32,3 @@ class Server(db.Model):
 
     def __repr__(self):
         return '<Server {}>'.format(self.id)
-
-
-class Session(db.Model):
-    __tablename__ = 'sessions'
-
-    id = db.Column(db.Integer, primary_key=True)
-    session_id = db.Column(db.String(255), unique=True)
-    data = db.Column(db.LargeBinary)
-    expiry = db.Column(db.DateTime)
-
-    def __init__(self, session_id, data, expiry):
-        self.session_id = session_id
-        self.data = data
-        self.expiry = expiry
