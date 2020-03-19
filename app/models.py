9da2b85ae54ea5d91c0ca69e5b0f699390975350
jude-lafitteIII <judewrs@gmail.com>
2020-03-19 20:55:03 +0000

    updated models to use new message system

--- app/models.py
+++ app/models.py
@@ -15,6 +15,8 @@ class Embed(db.Model):
 class Message(db.Model):
     __tablename__ = 'messages'
 
+    id = db.Column(db.Integer, primary_key=True)
+
     content = db.Column(db.String(2048), nullable=False, default='')
     embed = db.Column(db.Integer, db.ForeignKey(Embed.id))
 
