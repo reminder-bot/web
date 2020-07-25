1b4a87c19fc300517c8fa69d05219e8fb45b66ac
jude <judewrs@gmail.com>
2020-07-25 18:21:04 +0100

    thumbnails for embeds

--- app/routes/advanced_message_editor.py
+++ app/routes/advanced_message_editor.py
@@ -50,12 +50,14 @@ def update_message(guild_id: int, reminder_uid: str):
         else:
             footer_icon = icon if (icon := field('embed_footer_icon')).startswith('https://') else None
             image_url = icon if (icon := field('embed_image')).startswith('https://') else None
+            thumbnail_url = icon if (icon := field('embed_thumbnail')).startswith('https://') else None
 
             reminder.message.embed = Embed(
                 title=field('embed_title'),
                 description=field('embed_description'),
                 footer=field('embed_footer'),
                 image_url=image_url,
+                thumbnail_url=thumbnail_url,
                 footer_icon=footer_icon,
                 color=color.color)
 
